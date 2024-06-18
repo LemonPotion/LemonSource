2c7ce6da84c8c9b5a9fc62cc183f9a234d764a7e
LemonPotion <140953841+LemonPotion@users.noreply.github.com>
2024-06-18 21:51:36 +0300

    Pre release; Postgres added;

---
+++ LeMail.Persistence/Migrations/20240618184843_123.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeMail.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class _123 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
